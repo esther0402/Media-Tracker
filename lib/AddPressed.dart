3822bdfdb2ae2aa79d32dc361f9a3f459d63f063
aki <estherleejunkim@gmail.com>
2023-06-05 17:27:38 -0700

    Added a adding button
    Why is this so hard

---
+++ lib/AddPressed.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AddPressed extends StatefulWidget {
+  const AddPressed({Key? key}) : super(key: key);
+
+  @override
+  State<AddPressed> createState() {
+    return _AddPressedState();
+  }
+}
+
+class _AddPressedState extends State<AddPressed> {
+  int tasks = 0;
+
+  void addVariable() {
+    setState(() {
+      tasks++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(
+          onPressed: addVariable,
+          icon: const Icon(Icons.add),
+          color: Colors.amber,
+          iconSize: 50.0,
+          padding: const EdgeInsets.only(bottom: 40),
+        ),
+        Text('$tasks')
+      ],
+    );
+  }
+}
--- lib/BgContainer.dart
+++ lib/BgContainer.dart
@@ -1,4 +1,6 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
+import 'package:media_tracker/AddPressed.dart';
 
 class BgContainer extends StatelessWidget {
   const BgContainer({super.key});
@@ -17,24 +19,25 @@ class BgContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Column(children: [
-          const Padding(
-            padding: EdgeInsets.only(top: 50),
-            child: Text(
-              'Media Tracker',
-              style: TextStyle(
-                  fontStyle: FontStyle.italic,
-                  fontSize: 50,
-                  color: Color.fromARGB(255, 255, 255, 255)),
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 50),
+              child: Text(
+                'Media Tracker',
+                style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontSize: 50,
+                    color: Color.fromARGB(255, 255, 255, 255)),
+              ),
             ),
-          ),
-          Padding(
+            Padding(
               padding: const EdgeInsets.only(top: 50.0),
-              child: Container(
-                  width: 500,
-                  height: 800,
-                  color: Colors.white.withOpacity(0.8)))
-        ]),
+              child: Container(width: 500, height: 800, color: Colors.white),
+            ),
+            const AddPressed(),
+          ],
+        ),
       ),
     );
   }
